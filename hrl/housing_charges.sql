e5ff9281404af03d5086f74831662edc878d44fe
jdetok <jdeko17@gmail.com>
2026-09-15 10:48:34 -0500

    fixed housing charges

--- hrl/housing_charges.sql
+++ hrl/housing_charges.sql
@@ -1,8 +1,20 @@
 -- need the opposite as well (no housing charge, has 2)
-select spriden_id as bid, tbraccd_term_code as term, rorstat_pgrp_code as pgrp, rprarsc_actual_amt as rsrc
+select 
+    spriden_id as bid, 
+    tbraccd_term_code as term, 
+    rorstat_pgrp_code as pgrp, 
+    rprarsc_actual_amt as rsrc,
+    case when exists (
+        select 1 from sfrstcr
+        where sfrstcr_pidm = tbraccd_pidm
+        and sfrstcr_term_code = tbraccd_term_code
+        and not regexp_like(sfrstcr_rsts_code, '^D.|^W.')
+        and sfrstcr_bill_hr > 0
+    ) then 'Y' else 'N' end as regd
 from tbraccd
 join rorstat on rorstat_pidm = tbraccd_pidm 
     and rorstat_aidy_code = substr(tbraccd_term_code, 3, 2) - 1 || substr(tbraccd_term_code, 3, 2)
+    and rorstat_tgrp_code not in ('WITHDR', 'NAT')
 join spriden on spriden_pidm = tbraccd_pidm and spriden_change_ind is null
 join tbbdetc on tbbdetc_detail_code = tbraccd_detail_code
     and tbbdetc_type_ind = 'C'
@@ -14,15 +26,27 @@ left join rprarsc on rprarsc_pidm = tbraccd_pidm
     and rprarsc_term_code = tbraccd_term_code
     and rprarsc_arsc_code = 'RSLFAS'
 where tbraccd_term_code = '202710'
-and tbraccd_detail_code not in ('RLAF', 'HCBF', 'HCBS')
-group by spriden_id, tbraccd_term_code, rorstat_pgrp_code, rprarsc_actual_amt
+and tbraccd_detail_code not in ('RLAF', 'HCBF', 'HCBS', 'HFVL')
+group by spriden_id, tbraccd_pidm, tbraccd_term_code, rorstat_pgrp_code, rprarsc_actual_amt
 having sum(tbraccd_amount) > 0
 ;
 
-select spriden_id as bid, rorprst_period as term, rorstat_pgrp_code as pgrp, rprarsc_actual_amt as rsrc
+select 
+    spriden_id as bid, 
+    rorprst_period as term, 
+    rorstat_pgrp_code as pgrp, 
+    rprarsc_actual_amt as rsrc,
+    case when exists (
+        select 1 from sfrstcr
+        where sfrstcr_pidm = rorprst_pidm
+        and sfrstcr_term_code = rorprst_period
+        and not regexp_like(sfrstcr_rsts_code, '^D.|^W.')
+        and sfrstcr_bill_hr > 0
+    ) then 'Y' else 'N' end as regd
 from rorprst
 join rorstat on rorstat_pidm = rorprst_pidm 
     and rorstat_aidy_code = substr(rorprst_period, 3, 2) - 1 || substr(rorprst_period, 3, 2)
+    and rorstat_tgrp_code not in ('WITHDR', 'NAT')
 join spriden on spriden_pidm = rorprst_pidm and spriden_change_ind is null
 left join rprarsc on rprarsc_pidm = rorprst_pidm 
     and rprarsc_term_code = rorprst_period
@@ -36,7 +60,7 @@ and not exists (
         and tbbdetc_type_ind = 'C'
         and tbbdetc_dcat_code = 'HOU'
     where tbraccd_pidm = rorprst_pidm
-    and tbraccd_detail_code not in ('RLAF', 'HCBF', 'HCBS')
+    and tbraccd_detail_code not in ('RLAF', 'HCBF', 'HCBS', 'HFVL')
     and tbraccd_term_code = rorprst_period
     group by tbraccd_pidm, rorstat_pgrp_code, tbraccd_term_code
     having sum(tbraccd_amount) > 0
@@ -46,10 +70,22 @@ order by rprarsc_actual_amt
 
 -- PBI DYNAMIC TERM VERSIONS: 
 -- need the opposite as well (no housing charge, has 2)
-select spriden_id as bid, tbraccd_term_code as term, rorstat_pgrp_code as pgrp, rprarsc_actual_amt as rsrc
+select 
+    spriden_id as bid, 
+    tbraccd_term_code as term, 
+    rorstat_pgrp_code as pgrp, 
+    rprarsc_actual_amt as rsrc,
+    case when exists (
+        select 1 from sfrstcr
+        where sfrstcr_pidm = tbraccd_pidm
+        and sfrstcr_term_code = tbraccd_term_code
+        and not regexp_like(sfrstcr_rsts_code, '^D.|^W.')
+        and sfrstcr_bill_hr > 0
+    ) then 'Y' else 'N' end as regd
 from tbraccd
 join rorstat on rorstat_pidm = tbraccd_pidm 
     and rorstat_aidy_code = substr(tbraccd_term_code, 3, 2) - 1 || substr(tbraccd_term_code, 3, 2)
+    and rorstat_tgrp_code not in ('WITHDR', 'NAT')
 join spriden on spriden_pidm = tbraccd_pidm and spriden_change_ind is null
 join tbbdetc on tbbdetc_detail_code = tbraccd_detail_code
     and tbbdetc_type_ind = 'C'
@@ -61,15 +97,28 @@ left join rprarsc on rprarsc_pidm = tbraccd_pidm
     and rprarsc_term_code = tbraccd_term_code
     and rprarsc_arsc_code = 'RSLFAS'
 where tbraccd_term_code = '" & term & "'
-and tbraccd_detail_code not in ('RLAF', 'HCBF', 'HCBS')
-group by spriden_id, tbraccd_term_code, rorstat_pgrp_code, rprarsc_actual_amt
+and tbraccd_detail_code not in ('RLAF', 'HCBF', 'HCBS', 'HFVL')
+group by spriden_id, tbraccd_pidm, tbraccd_term_code, rorstat_pgrp_code, rprarsc_actual_amt
 having sum(tbraccd_amount) > 0
 ;
 
-select spriden_id as bid, rorprst_period as term, rorstat_pgrp_code as pgrp, rprarsc_actual_amt as rsrc
+-- without housing
+select 
+    spriden_id as bid, 
+    rorprst_period as term, 
+    rorstat_pgrp_code as pgrp, 
+    rprarsc_actual_amt as rsrc,
+    case when exists (
+        select 1 from sfrstcr
+        where sfrstcr_pidm = rorprst_pidm
+        and sfrstcr_term_code = rorprst_period
+        and not regexp_like(sfrstcr_rsts_code, '^D.|^W.')
+        and sfrstcr_bill_hr > 0
+    ) then 'Y' else 'N' end as regd
 from rorprst
 join rorstat on rorstat_pidm = rorprst_pidm 
     and rorstat_aidy_code = substr(rorprst_period, 3, 2) - 1 || substr(rorprst_period, 3, 2)
+    and rorstat_tgrp_code not in ('WITHDR', 'NAT')
 join spriden on spriden_pidm = rorprst_pidm and spriden_change_ind is null
 left join rprarsc on rprarsc_pidm = rorprst_pidm 
     and rprarsc_term_code = rorprst_period
@@ -83,7 +132,7 @@ and not exists (
         and tbbdetc_type_ind = 'C'
         and tbbdetc_dcat_code = 'HOU'
     where tbraccd_pidm = rorprst_pidm
-    and tbraccd_detail_code not in ('RLAF', 'HCBF', 'HCBS')
+    and tbraccd_detail_code not in ('RLAF', 'HCBF', 'HCBS', 'HFVL')
     and tbraccd_term_code = rorprst_period
     group by tbraccd_pidm, rorstat_pgrp_code, tbraccd_term_code
     having sum(tbraccd_amount) > 0
@@ -91,4 +140,10 @@ and not exists (
 order by rprarsc_actual_amt
 ;
 
+select * from sfrstcr
+where sfrstcr_pidm = 1437763
+and sfrstcr_term_code = '202710'
+and not regexp_like(sfrstcr_rsts_code, '^D.|^W.')
+and sfrstcr_bill_hr > 0;
 
+select * from stvrsts where stvrsts_code = 'WW';
